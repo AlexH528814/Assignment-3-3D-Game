98ffd2aa3d28babe9bba343c4c2ece4afb3a7318
Alex Huddle <528814@escg.ac.uk>
2022-12-07 15:53:17 +0000

    episode 8 done

--- 3D RPG tutorial/Assets/Scripts/EquipmentManager.cs
+++ 3D RPG tutorial/Assets/Scripts/EquipmentManager.cs
@@ -17,7 +17,10 @@ public class EquipmentManager : MonoBehaviour
 
     #endregion
 
+    public Equipment[] defaultItems;
+    public SkinnedMeshRenderer targetMesh;
     Equipment[] currentEquipment;
+    SkinnedMeshRenderer[] currentMeshes;
 
     public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
     public OnEquipmentChanged onEqupmentChanged;
@@ -31,34 +34,57 @@ public class EquipmentManager : MonoBehaviour
         int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
 
         currentEquipment = new Equipment[numSlots];
+        currentMeshes = new SkinnedMeshRenderer[numSlots];
+
+        EquipDefaultItems();
     }
 
     public void Equip(Equipment newItem)
     {
         int slotIndex = (int)newItem.equipSlot;
+        
+        Equipment oldItem = Unequip(slotIndex);
 
-        Equipment oldItem = null;
 
-        if (currentEquipment[slotIndex] != null)
-        {
-            oldItem = currentEquipment[slotIndex];
-
-            inventory.Add(oldItem);
-        }
 
         if (onEqupmentChanged != null)
         {
             onEqupmentChanged.Invoke(newItem, oldItem);
         }
 
+        SetEquipmentBlendShapes(newItem, 100);
+
         currentEquipment[slotIndex] = newItem;
+
+        SkinnedMeshRenderer newMesh = Instantiate<SkinnedMeshRenderer>(newItem.mesh);
+        newMesh.transform.parent = targetMesh.transform;
+
+        newMesh.bones = targetMesh.bones;
+        newMesh.rootBone = targetMesh.rootBone;
+        currentMeshes[slotIndex] = newMesh;
+
     }
 
-    public void Unequip(int slotIndex)
+    void EquipDefaultItems()
+    {
+        foreach (Equipment item in defaultItems)
+        {
+            Equip(item);
+        }
+    }
+
+    public Equipment Unequip(int slotIndex)
     {
         if (currentEquipment[slotIndex] != null)
         {
+            if (currentMeshes[slotIndex] != null)
+            {
+                Destroy(currentMeshes[slotIndex].gameObject);
+            }
+
+
             Equipment oldItem = currentEquipment[slotIndex];
+            SetEquipmentBlendShapes(oldItem, 0);
             inventory.Add(oldItem);
 
             currentEquipment[slotIndex] = null;
@@ -67,7 +93,10 @@ public class EquipmentManager : MonoBehaviour
             {
                 onEqupmentChanged.Invoke(null, oldItem);
             }
+
+            return oldItem;
         }
+        return null;
     }
 
     public void UnequipAll()
@@ -76,11 +105,21 @@ public class EquipmentManager : MonoBehaviour
         {
             Unequip(i);
         }
+
+        EquipDefaultItems();
+    }
+
+    void SetEquipmentBlendShapes(Equipment item, int weight)
+    {
+        foreach (EquipmentMeshRegion blendShape in item.coveredMeshRegions)
+        {
+            targetMesh.SetBlendShapeWeight((int)blendShape, weight);
+        }
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("UnequipItems"))
+        if (Input.GetButtonDown("Unequip"))
         {
             UnequipAll();
         }
